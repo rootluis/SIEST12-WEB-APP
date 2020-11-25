3e02d90bb5a79f60027edf44d11327e6771374d5
Luis <rootluis85@gmail.com>
2020-11-24 20:33:38 -0600

    Estructura inicial del proyecto SpringBoot - siest12-web-app

---
+++ src/main/java/com/edu/mx/web/est/siest12/Siest12WebAppApplication.java
@@ -0,0 +1,13 @@
+package com.edu.mx.web.est.siest12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Siest12WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Siest12WebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/mx/web/est/siest12/Siest12WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.mx.web.est.siest12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Siest12WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
